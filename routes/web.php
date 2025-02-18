a8e688280dd08f09b6b7402dd114208145ae8bfc
Lukas <lukasfidencio.dev@gmail.com>
2025-02-17 22:18:20 -0300

    001

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/oi', function () {
+    echo "welcome";
+});
+
+Route::get('/about', function () {
+    echo "sobre nózes.";
+});
